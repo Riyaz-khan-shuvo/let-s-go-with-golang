9bd6d3f2ff75a2b205f59fb3baa09e58ed647bad
Riyaz-khan-shuvo <mdriyaz5965@gmail.com>
2022-03-24 12:36:37 +0600

    if else statement complete

---
+++ 18-if-else/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("The if else statement !!!")
+
+	if age := 19; age < 18 {
+		fmt.Println("Children!!!")
+	} else {
+		fmt.Println("Adult!!!")
+	}
+
+}
