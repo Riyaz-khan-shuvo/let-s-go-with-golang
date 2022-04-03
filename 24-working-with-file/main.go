5a0991d1a9c058f6827d22e522cd7ea57f345215
Riyaz-khan-shuvo <mdriyaz5965@gmail.com>
2022-04-03 14:36:35 +0600

    read and write file complete

--- 24-working-with-file/main.go
+++ 24-working-with-file/main.go
@@ -3,23 +3,31 @@ package main
 import (
 	"fmt"
 	"io"
+	"io/ioutil"
 	"os"
 )
 
 func main() {
 	fmt.Println("Welcome to the files in golang!!!")
-
 	content := "I am learning Go lang."
 	files, err := os.Create("./gotext.txt")
-	if err != nil {
-		panic(err)
-	}
+	checkNilError(err)
 	length, err := io.WriteString(files, content)
+	checkNilError(err)
+	fmt.Println("Length is : ", length)
+	defer files.Close()
+	readFile("./gotext.txt")
+
+}
+
+func readFile(fileName string) {
+	databyte, err := ioutil.ReadFile(fileName)
+	checkNilError(err)
+	fmt.Println("The data of this file is : ", string(databyte))
+}
+
+func checkNilError(err error) {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("Length is : ", length)
-
-	defer files.Close()
-
 }
